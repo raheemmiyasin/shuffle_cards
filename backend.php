1a07af37689dd7511531baec0811e2f120954d46
bameng <raheemmi@aerohub.co>
2019-03-18 03:30:20 +0800

    init commit

---
+++ backend.php
@@ -0,0 +1,73 @@
+<?php
+$responseObject = new stdClass();
+try {
+
+//sanitize input
+    $personCount = isset($_POST['person_no']) ? $_POST['person_no'] : null;
+
+    //validate input
+    if ($personCount < 0 || $personCount == null || is_nan($personCount)) {
+
+        //prepare exception response for error to frontend
+        $responseObject->status = "Error";
+        $responseObject->message = "Input value does not exist or value is invalid";
+        echo json_encode($responseObject);
+        exit;
+    }
+
+    //building cards
+    $types = array('S', 'H', 'D', 'C');
+
+    $values = array('A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K');
+
+    $cards = array();
+
+    foreach ($types as $type) {
+
+        foreach ($values as $value) {
+
+            array_push($cards, $type . '-' . $value);
+        }
+
+    }
+
+//prepare persons hands
+    $personCards = array();
+
+//shuffle cards
+    shuffle($cards);
+
+//distribute cards to each persons hands
+
+    $currentCard = 0;
+
+    while ($currentCard < sizeof($cards)) {
+        for ($j = 0; $j < $personCount; $j++) {
+            if (isset($personCards[$j])) {
+                $personCards[$j] = $personCards[$j] . ", " . $cards[$currentCard];
+            } else {
+                $personCards[$j] = $cards[$currentCard];
+            }
+            $currentCard++;
+
+            //stop once all card distributed
+            if ($currentCard == sizeof($cards)) {
+                break;
+            }
+        }
+    }
+
+    //prepare response for success to frontend
+    $responseObject->data = $personCards;
+    $responseObject->status = "Success";
+    $responseObject->message = "Success";
+    echo json_encode($responseObject);
+
+} catch (Exception $e) {
+
+    //prepare exception response for error to frontend
+    $responseObject->status = "Error";
+    $responseObject->message = "Irregularity occurred";
+    echo json_encode($responseObject);
+    exit;
+}
